45058356bc4c36146f2f5fbcc09879caf78af176
Ruben_Thomas <rubenethomas1@gmail.com>
2025-05-01 06:23:09 -0600

    started on end function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
-        return file_contents
+        return print(file_contents)
 
-    
-    
 
-from stats import number_of_words, count_of_characters
+
+from stats import end, number_of_words, count_of_characters
+word_count = number_of_words()
+characters = count_of_characters()
 number_of_words()
 count_of_characters()
-
+end()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+
+
 def number_of_words():
     word_count = 0
     with open("/home/ruben_thomas/workspace/github.com/bootdotdev/curriculum/bookbot/books/frankenstein.txt") as f:
@@ -6,7 +8,9 @@ def number_of_words():
     words = file_contents.split()
     for word in words:
         word_count += 1
-    return print(f"{word_count} words found in the document")
+    return word_count
+
+word_count = number_of_words()
 
 def count_of_characters():
     characters = {}
@@ -22,6 +26,28 @@ def count_of_characters():
                 characters[letter] += 1
             else:
                 characters[letter] = 1
-    return print(characters)
+    return characters
 
+characters = count_of_characters()
 
+def sort(dict):
+    return 
+
+def end(characters,word_count): 
+    list_of_characters = []
+    for character in characters:
+        if character.isalpha() == True:
+            list_of_characters.append(character)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    
+    list_of_characters.sort(key=None, reverse=False)
+    for list_of_character in list_of_characters:
+        print(f"{list_of_character} : {characters[list_of_character]}")
+    print("============= END ===============") 
+    print(characters)
+    return
+end(characters,word_count)
